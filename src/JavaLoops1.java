1ff7118da4804c2c587b107001eb02e64444df9c
ulvglzd <ulviguluzada@gmail.com>
2023-02-17 16:21:25 +0400

    Solved successfully

--- src/JavaLoops1.java
+++ src/JavaLoops1.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class JavaLoops1 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class JavaLoops1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+
+        bufferedReader.close();
+
+        if (N >= 2 && N <= 20){
+            for (int i = 1; i < 11; i++){
+                int result = i * N;
+                System.out.printf("%d x %d = %d%n", N, i, result);
+            }
+        }
+    }
 }
